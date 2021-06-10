eb9ca3fe4b49a3a27f64ff09e4c0c5fa0e8f1182
Tierra Rogers <tierragore@yahoo.com>
2021-06-10 02:38:55 -0400

    Updating Readme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render( <App />,
   document.getElementById('root')
