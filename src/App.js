71bdc73b9c49d66d0a25dbc110c4488a2e6aebb9
Tierra Rogers <tierragore@yahoo.com>
2021-06-10 02:10:13 -0400

    Updating files

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import NavBar from './components/NavBar/NavBar.js';
 import About from './components/About/About.js';
 import Home from './components/Home/Home.js';
 import Skills from './components/Skills/Skills.js';
+import Projects from './components/Projects/Projects.js';
 import Contact from './components/Contact/Contact.js';
 
 const App = ()  => {
   return (
     <div className="App">
      <NavBar />
-     <About />
      <Home />
+     <About />
      <Skills />
+     <Projects />
      <Contact />
     </div>
   );
