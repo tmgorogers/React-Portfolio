2e3458c774b207bfd1e828285e338a9c72dafdf8
Tierra Rogers <tierragore@yahoo.com>
2021-06-09 21:47:31 -0400

    fixed About.js bug

---
+++ portfolio/src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import classes from './Home.module.css'
+import code from './../../img/code.svg'
+
+
+
+const Home = () => {
+    return (
+        <div className={classes.Home}>
+            <div className={classes.Container}>
+                <h1 className={classes.Hello}>Hello, world</h1>
+                <h1> Welcome on my website </h1>
+            </div>
+            <img className={classes.Code}src={code} alt="code"></img>
+            
+        </div>
+    );
+}
+
+export default Home;
